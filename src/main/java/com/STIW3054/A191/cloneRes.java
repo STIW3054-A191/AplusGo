1aec5cf204cc6d525b9747d794de5cfb98986228
Wang Luo <55168186+WwLuo-1024@users.noreply.github.com>
2019-12-19 14:48:24 +0800

    251230 Update

--- src/main/java/com/STIW3054/A191/cloneRes.java
+++ src/main/java/com/STIW3054/A191/cloneRes.java
@@ -1,87 +1,110 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-    <groupId>com.mycompany</groupId>
-    <artifactId>RealTimeAssignemnt</artifactId>
-    <version>1.0-SNAPSHOT</version>
-    <packaging>jar</packaging>
-    <dependencies>
-        <dependency>
-            <groupId>org.jsoup</groupId>
-            <artifactId>jsoup</artifactId>
-            <version>1.12.1</version>
-            <type>jar</type>
-        </dependency>
-        <dependency>
-            <groupId>org.apache</groupId>
-            <artifactId>apache</artifactId>
-            <version>7</version>
-            <type>pom</type>
-        </dependency>
-        <!-- https://mvnrepository.com/artifact/org.wso2.apache.httpcomponents/httpclient -->
-<dependency>
-    <groupId>org.wso2.apache.httpcomponents</groupId>
-    <artifactId>httpclient</artifactId>
-    <version>4.3.1.wso2v1</version>
-</dependency>
-<dependency>
-        <groupId>org.projectlombok</groupId>
-        <artifactId>lombok</artifactId>
-        <version>1.16.10</version>
-    </dependency>
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author WANG LUO
+ */
+public class CloneRes extends Thread{
+//    public static void main(String[] args) throws IOException{
+//        String command ="git clone https://github.com/WwLuo-1024/251230-STIW3054-A191-A1";
+//        Process p = Runtime.getRuntime().exec(command);
+//        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//        
+//        }
     
-   <!-- https://mvnrepository.com/artifact/org.apache.poi/poi -->
-<dependency>
-    <groupId>org.apache.poi</groupId>
-    <artifactId>poi</artifactId>
-    <version>4.1.0</version>
-</dependency>
-
-    <dependency>
-        <groupId>org.apache.poi</groupId>
-        <artifactId>poi-ooxml</artifactId>
-        <version>3.10-FINAL</version>
-    </dependency>
+    public boolean mkDirectory(String path){
+        File file = null;
+        try{
+            file = new File(path);
+            if(!file.exists()){
+                return file.mkdirs();
+            }
+            else{
+                return false;
+            }
+        }catch(Exception e){           
+        }finally{
+            file = null;
+        }
+        return false;
+    }
     
-    <!-- https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit -->
-    <dependency>
-    <groupId>org.eclipse.jgit</groupId>
-    <artifactId>org.eclipse.jgit</artifactId>
-    <version>5.6.0.201912101111-r</version>
-    </dependency>
+    @Override
+    public void run(){
+        GetLink gl = new GetLink();
+        List<String> urlList=new ArrayList<String>();
+        urlList.addAll(gl.getLinksByJsoup());
+        for(int i = 0;i<urlList.size();i++){
+            try {
+                String full_command;
+                String command = "git clone {}";
+                full_command = command.replace("{}",urlList.get(i));
+                
+                Process p = Runtime.getRuntime().exec(full_command,null,new File("D:\\realDemo"));
+            } catch (IOException ex) {
+                Logger.getLogger(CloneRes.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            } 
+    }
     
-    <!-- https://mvnrepository.com/artifact/com.googlecode.json-simple/json-simple -->
-    <dependency>
-    <groupId>com.googlecode.json-simple</groupId>
-    <artifactId>json-simple</artifactId>
-    <version>1.1</version>
-    </dependency>
-
-
-
-    <!-- https://mvnrepository.com/artifact/org.json/json -->
-    <dependency>
-    <groupId>org.json</groupId>
-    <artifactId>json</artifactId>
-    <version>20180813</version>
-    </dependency>
-    
-    <!-- https://mvnrepository.com/artifact/com.itextpdf/itextpdf -->
-    <dependency>
-    <groupId>com.itextpdf</groupId>
-    <artifactId>itextpdf</artifactId>
-    <version>5.5.13</version>
-    </dependency>
-
-
-    
-    </dependencies>
-    
-    <properties>
-        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
-        <maven.compiler.source>1.8</maven.compiler.source>
-        <maven.compiler.target>1.8</maven.compiler.target>
-    </properties>
- 
-</project>
+    public void Clone() throws IOException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        Future<Integer> result = executor.submit(task);
+        executor.shutdown();
+        try {                   
+            Thread.sleep(1000);
+            CloneRes a = new CloneRes();
+            a.start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }        
+        System.out.println("The main thread is performing tasks");
+         
+        try {
+            System.out.println("There are "+result.get()+" files need to be downloaded for this task");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }         
+        System.out.println("All repositories have been successfully cloned ");
+    }
+}
+class Task implements Callable<Integer>{
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("The Main Thread Start");
+        Thread.sleep(3000);
+        String mkDirectoryPath = "D:\\realDemo";
+        CloneRes cr = new CloneRes();
+        if(cr.mkDirectory(mkDirectoryPath)){
+            System.out.println(mkDirectoryPath+" The Direcory have benn sucessfully created");
+        }
+        else{
+            System.out.println(mkDirectoryPath+" The Directory created failed!The Directory have been already existed");
+        }
+        int resNumber = 0;
+        GetLink gl = new GetLink();
+        for(int i=0;i<gl.getLinksByJsoup().size();i++)
+            resNumber += 1;
+        return resNumber;
+    }
+    }
